22b36d5636f9ecf844d28f6f402cecf0bede0f8b
andes2912 <andridesmana29@gmail.com>
2021-04-29 02:29:26 +0700

    Add vue in frontend

--- config/app.php
+++ config/app.php
@@ -164,8 +164,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        App\Providers\RupiahServiceProvider::class,
-
         /*
          * Package Service Providers...
          */
