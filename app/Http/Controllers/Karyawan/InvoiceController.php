c370d169d7bc6d5fcf316df02cb2119f645a989e
andes2912 <andridesmana29@gmail.com>
2021-05-11 12:38:14 +0700

    Move invoice Code to InvoiceController

--- app/Http/Controllers/Karyawan/InvoiceController.php
+++ app/Http/Controllers/Karyawan/InvoiceController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers\Karyawan;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\{transaksi};
+use Auth;
 class InvoiceController extends Controller
 {
        // Invoice
