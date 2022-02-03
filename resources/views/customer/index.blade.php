1c149266793e89036ff2dda21b8d9333ef1d91eb
andes2912 <andridesmana29@gmail.com>
2022-02-03 14:19:36 +0700

    Improve dashboard customer

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -27,12 +27,12 @@
                           <div class="media">
                               <div class="avatar bg-light-primary mr-2">
                                   <div class="avatar-content">
-                                      <i class="feather icon-box text-primary font-medium-5"></i>
+                                      <i class="feather icon-check text-primary font-medium-5"></i>
                                   </div>
                               </div>
                               <div class="media-body my-auto">
-                                  <h4 class="font-weight-bolder mb-0">{{$totalLaundry}}</h4>
-                                  <p class="card-text font-small-1 mb-0">Jumlah Laundry Masuk</p>
+                                  <h4 class="font-weight-bolder mb-0">{{$totalLaundry}} Total</h4>
+                                  <p class="card-text font-small-1 mb-0">Jumlah Laundry</p>
                               </div>
                           </div>
                       </div>
@@ -44,8 +44,8 @@
                                   </div>
                               </div>
                               <div class="media-body my-auto">
-                                  <h4 class="font-weight-bolder mb-0">{{$totalLaundryKg}}</h4>
-                                  <p class="card-text font-small-1 mb-0">Jumlah Laundry Kg</p>
+                                  <h4 class="font-weight-bolder mb-0">{{$totalLaundryKg}} Kg</h4>
+                                  <p class="card-text font-small-1 mb-0">Jumlah Laundry</p>
                               </div>
                           </div>
                       </div>
@@ -53,7 +53,7 @@
                           <div class="media">
                               <div class="avatar bg-light-danger mr-2">
                                   <div class="avatar-content">
-                                      <i class="feather icon-check text-danger font-medium-5"></i>
+                                      <i class="feather icon-star text-danger font-medium-5"></i>
                                   </div>
                               </div>
                               <div class="media-body my-auto">
@@ -66,5 +66,83 @@
               </div>
           </div>
       </div>
+
+      <div class="col-xl-12 col-md-12 col-12">
+        {{-- Table --}}
+        <div class="card">
+          <div class="card-body">
+            <h4 class="card-title">
+              Data Transaksi Kamu
+            </h4>
+            <div class="table-responsive m-t-0">
+              <table id="myTable" class="table display table-bordered table-striped">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>No Resi</th>
+                    <th>TGL Transaksi</th>
+                    <th>Status</th>
+                    <th>Payment</th>
+                    <th>Jenis</th>
+                    <th>Total</th>
+                    {{-- <th>Action</th> --}}
+                  </tr>
+                </thead>
+                <tbody>
+                  @foreach ($transaksi as $key => $transaksis)
+                    <tr>
+                      <td> {{$key+1}} </td>
+                      <td> {{$transaksis->invoice}} </td>
+                      <td> {{$transaksis->tgl_transaksi}} </td>
+                      <td> {{$transaksis->status_order}} </td>
+                      <td> {{$transaksis->status_payment}} </td>
+                      <td> {{$transaksis->price->jenis}} </td>
+                      <td> {{$transaksis->harga_akhir}} </td>
+                      {{-- <td>
+                        <a href="" class="btn btn-sm btn-info">Detail</a>
+                      </td> --}}
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
  </div>
+@endsection
+@section('scripts')
+<script type="text/javascript">
+// DATATABLE
+$(document).ready(function() {
+    $('#myTable').DataTable();
+    $(document).ready(function() {
+        var table = $('#example').DataTable({
+            "columnDefs": [{
+                "visible": false,
+                "targets": 2
+            }],
+            "order": [
+                [2, 'asc']
+            ],
+            "displayLength": 25,
+            "drawCallback": function(settings) {
+                var api = this.api();
+                var rows = api.rows({
+                    page: 'current'
+                }).nodes();
+                var last = null;
+                api.column(2, {
+                    page: 'current'
+                }).data().each(function(group, i) {
+                    if (last !== group) {
+                        $(rows).eq(i).before('<tr class="group"><td colspan="5">' + group + '</td></tr>');
+                        last = group;
+                    }
+                });
+            }
+        });
+    });
+});
+</script>
 @endsection
