e791acf7427f302f404f3846e6ce6ec6214830d8
andes2912 <andridesmana29@gmail.com>
2022-02-03 10:15:56 +0700

    Queue send email customer

---
+++ app/Jobs/RegisterCustomerJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\RegisterCustomer;
+use Mail;
+
+class RegisterCustomerJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $data;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+      $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+      $email = new RegisterCustomer($this->data);
+      Mail::to($this->data['email'])->send($email);
+    }
+}
