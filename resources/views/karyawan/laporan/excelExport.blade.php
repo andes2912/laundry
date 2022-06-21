a82aebc74de8fd2e5238ca6caa0ccf4c07ab5087
andes2912 <andridesmana29@gmail.com>
2022-06-21 11:59:15 +0700

    Improve Excel Report

--- resources/views/karyawan/laporan/excelExport.blade.php
+++ resources/views/karyawan/laporan/excelExport.blade.php
@@ -24,14 +24,26 @@
         </tr>
     </thead>
     <tbody>
-       @foreach ($data as $key => $items)
-          <tr>
-            <td> {{$key+1}}  </td>
-            <td colspan="2"> {{$items->customer}} </td>
-            <td colspan="2"> {{$items->price->jenis}} </td>
-            <td colspan="2"> {{$items->jenis_pembayaran}} </td>
-            <td colspan="2"> {{$items->harga_akhir}} </td>
-          </tr>
+      @php
+          $grand = 0;
+      @endphp
+      @foreach ($data as $key => $items)
+        <tr>
+          <td> {{$key+1}}  </td>
+          <td colspan="2"> {{$items->customer}} </td>
+          <td colspan="2"> {{$items->price->jenis}} </td>
+          <td colspan="2"> {{$items->jenis_pembayaran}} </td>
+          <td colspan="2">Rp {{number_format($items->harga_akhir)}} </td>
+        </tr>
+        @php
+          $grand += $items->harga_akhir
+        @endphp
        @endforeach
     </tbody>
+    <tr>
+      <td colspan="5" rowspan="1" style="border-left-style: hidden;"></td>
+      <td style="border-bottom-style: hidden; border-left-style: hidden; "></td>
+      <td style="font-weight: bold; text-align:right">GRAND TOTAL</td>
+      <td style="text-align:right">Rp {{number_format($grand)}} </td>
+    </tr>
 </table>
